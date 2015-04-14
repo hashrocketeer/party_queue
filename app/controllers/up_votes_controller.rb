40e7c1ca14309d79270c540e23e04e3c6182ee5f
Hashrocket Workstation <dev@hashrocket.com>
2015-04-14 09:59:06 -0400

    Uses JS to reorder playlist after vote is cast

--- app/controllers/up_votes_controller.rb
+++ app/controllers/up_votes_controller.rb
@@ -4,7 +4,9 @@ class UpVotesController < ApplicationController
   def create
     if valid_up_vote(track)
       VoteProcessingService.new(track, current_music_queue, current_google_user).process_up_vote
-      redirect_to music_queue_path(current_music_queue)
+      unless request.xhr?
+        redirect_to music_queue_path(current_music_queue)
+      end
     else
       redirect_to music_queue_path(current_music_queue), notice: 'You can only place one +1 vote.'
     end
